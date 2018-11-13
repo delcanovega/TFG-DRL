7df10e0a296ecf2ec6ad7041a003284fbe3cd0cb
delcanovega <delcanovega@gmail.com>
2018-11-13 20:53:35 +0100

    QL-Agent: buckets changed for more stable performance

--- 01_CartPole/rl_agent.py
+++ 01_CartPole/rl_agent.py
@@ -3,37 +3,34 @@ import random
 import math
 
 # Hyperparameters
-# TODO: improve - maybe gridsearch-like method?
-LEARNING_RATE = 0.1
-DISCOUNT_FACTOR = 1.0
-EXPLORATION = 0.5
-MIN_EXPLORATION = 0.01
+LEARNING_RATE = 0.1     # Alpha
+DISCOUNT_FACTOR = 1.0   # Gamma
+EXPLORATION = 0.5       # Epsilon (initial)
+MIN_EXPLORATION = 0.01  # Epsilon (final)
+
 
 class QLAgent:
     def __init__(self, state_space, action_space):
         self.action_space = action_space
         self.state_space = state_space
+
+        # Bounds: [cart_position, cart_velocity, pole_angle, pole_velocity]
         self.upper_bounds = [self.state_space.high[0], 0.5, self.state_space.high[2], math.radians(50)]
         self.lower_bounds = [self.state_space.low[0], -0.5, self.state_space.low[2], -math.radians(50)]
 
         self.exploration = EXPLORATION
 
-        self.buckets = (1, 1, 6, 12)  # TODO: try out other values
+        self.buckets = (1, 1, 3, 6)
         self.n_states = 1
         for i in self.buckets:
             self.n_states *= i
 
-        #self.q_table = np.zeros([self.n_states, self.action_space])
         self.q_table = np.zeros(self.buckets + (self.action_space,))
 
     def discretize(self, obs):
         ratios = [(obs[i] + self.upper_bounds[i]) / (self.upper_bounds[i] - self.lower_bounds[i]) for i in range(len(obs))]
         classified_obs = [int(round((self.buckets[i] - 1) * ratios[i])) for i in range(len(obs))]
         classified_obs = [min(self.buckets[i] - 1, max(0, classified_obs[i])) for i in range(len(obs))]
-
-        #state = 0
-        #for i in range(len(classified_obs)):
-        #    state += classified_obs[i] if i == 0 else (classified_obs[i]) * self.buckets[i-1]
 
         return tuple(classified_obs)
 
@@ -55,10 +52,8 @@ class QLAgent:
         next_max = np.max(self.q_table[discrete_next_state])
 
         new_value = (1 - LEARNING_RATE) * old_value + LEARNING_RATE * (reward + DISCOUNT_FACTOR * next_max)
-        #self.q_table[discrete_old_state, action] = new_value
         self.q_table[discrete_old_state][action] = new_value
 
     def decrease_exploration(self):
-        # TODO: reduce the learning rate and exploration
         if self.exploration > MIN_EXPLORATION:
             self.exploration = self.exploration * 0.99
