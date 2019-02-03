9c33bd904152291ead3efe5de4883a9bd6c67e65
Unknown <juanluromero8@gmail.com>
2019-02-03 20:33:09 +0100

    CartPole NN - Working but not learning

--- 01_CartPole/NN_CartPole.py
+++ 01_CartPole/NN_CartPole.py
@@ -9,9 +9,15 @@ from collections import deque
 
 MEMORY_SIZE = 2000
 EXPLORATION_MIN = 0.1
+HIDDEN_LAYER_SIZE = 16
+
+# HYPERPARAMETERS
+LEARNING_RATE = 1.0
+DISCOUNT_FACTOR = 0.95
+EXPLORATION = 1.0
 
 class DQNAgent:
-    def __init__(self, state_space, action_space, learning_rate, discount_factor, exploration):
+    def __init__(self, state_space, action_space):
         self.state_space = state_space
         self.action_space = action_space
 
@@ -19,49 +25,48 @@ class DQNAgent:
         self.memory = deque(maxlen=MEMORY_SIZE)
 
         # Hyperparameters
-        self.learning_rate = learning_rate
-        self.discount_factor = discount_factor
-        self.exploration = exploration
-
-        self.epsilon_min = 0.01
+        self.learning_rate = LEARNING_RATE
+        self.discount_factor = DISCOUNT_FACTOR
+        self.exploration = EXPLORATION
 
         # Neural Network structure
         self.model = Sequential()
         # Input Layer of state size(4) and Hidden Layer with 16 nodes
-        self.model.add(Dense(16, input_dim=self.state_space, activation='relu'))
+        self.model.add(Dense(24, input_dim=self.state_space, activation='relu'))
         # Hidden layer with 16 nodes
-        self.model.add(Dense(units=16, activation='linear'))
+        self.model.add(Dense(24, activation='relu'))
         # Output Layer with # of actions: 2 nodes (left, right)
         self.model.add(Dense(self.action_space, activation='linear'))
 
         self.model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
 
     def predict_action(self, state):
-        if random.uniform(0, 1) < self.exploration:
+        if np.random.rand() < self.exploration:
             # Explore
             action = random.randrange(self.action_space)
         else:
             # Exploit
-            action_value = self.model.predict(np.array([state]))[0]  # Best action
-            action = np.argmax(action_value)
-
+            action_value = self.model.predict(state)
+            action = np.argmax(action_value[0])
         return action
 
-    def update_table(self, observation):
-        self.memory.append(observation)
+    def update_table(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
 
     def replay(self, batch_size):
+        if len(self.memory) < batch_size:
+            return
+        
         minibatch = random.sample(self.memory, batch_size)
 
         for state, action, reward, next_state, done in minibatch:
             target = reward
             if not done:
-                target = (reward + self.discount_factor * np.amax(self.model.predict(next_state)[0]))
+                target = reward + self.discount_factor * np.amax(self.model.predict(next_state)[0])
             target_f = self.model.predict(state)
             target_f[0][action] = target
             self.model.fit(state, target_f, epochs=1, verbose=0)
         
         if self.exploration > EXPLORATION_MIN:
-            self.exploration *= 0.99
+            self.exploration *= 0.995
 
-    # TODO: add get_minibatch ad replay if necessary
--- 01_CartPole/main.py
+++ 01_CartPole/main.py
@@ -1,4 +1,4 @@
-# coding=UTF-8
+#coding=UTF-8
 
 # Imports
 import gym
@@ -10,19 +10,20 @@ from collections import deque
 from NN_CartPole import DQNAgent
 
 # Hyperparameters
-LEARNING_RATE = 1
-EXPLORATION = 0.9
-DISCOUNT_FACTOR = 0.1
+LEARNING_RATE = 1.0
+EXPLORATION = 1.0
+DISCOUNT_FACTOR = 0.95
 
 EPISODES = 500
 
 if __name__ == '__main__':
     env = gym.make('CartPole-v1')
 
-    agent = DQNAgent(env.observation_space, env.action_space.n, LEARNING_RATE, DISCOUNT_FACTOR, EXPLORATION)
+    agent = DQNAgent(env.observation_space.shape[0], env.action_space.n)
 
     for i in range(EPISODES):
         state = env.reset()
+        state = np.reshape(state, [1, env.observation_space.shape[0]]) # TODO: check this
 
         done = False
         acc_reward = 0
@@ -34,14 +35,12 @@ if __name__ == '__main__':
             action = agent.predict_action(state)
 
             next_state, reward, done, info = env.step(action)
-            observation = state, next_state, action, reward
-            agent.update_table(observation)
-
+            next_state = np.reshape(next_state, [1, env.observation_space.shape[0]]) # TODO: check this
+            agent.update_table(state, action, reward, next_state, done)
             state = next_state
             acc_reward += reward
 
-        print("Episode {}/{} ended with score {}".format(i, EPISODES, acc_reward))
-
+        print("Episode {}/{} ended with score {}".format(i+1, EPISODES, acc_reward))
         agent.replay(32)
 
         # if i % 100 == 0 and i > 0:
