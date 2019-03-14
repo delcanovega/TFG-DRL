1939951c3d598a83a902448869637618e22822fc
delcanovega <delcanovega@gmail.com>
2019-03-14 13:36:00 +0100

    SimpleAgent & BatchAgent updated

--- 03_CartPole_NN/drl_agent.py
+++ 03_CartPole_NN/drl_agent.py
@@ -7,21 +7,20 @@ from keras.optimizers import Adam
 from keras.initializers import Constant
 
 from collections import deque
-from itertools import chain
 
 
 # HYPERPARAMETERS
 
 # Reinforcement Learning:
-LEARNING_RATE = 0.1         # Alpha (try higher values for minibatch training)
+LEARNING_RATE = 0.001         # Alpha (try higher values for minibatch training)
 DISCOUNT_FACTOR = 0.95      # Gamma
-EXPLORATION = 0.5           # Epsilon (initial)
+EXPLORATION = 1.0           # Epsilon (initial)
 MIN_EXPLORATION = 0.01      # Epsilon (final)
 
 # Agent:
 MEMORY_SIZE = 2000
-HIDDEN_LAYER_SIZE = 12
-MINIBATCH_SIZE = 100
+HIDDEN_LAYER_SIZE = 24
+MINIBATCH_SIZE = 20
 
 
 class DQNAgent:
@@ -47,9 +46,7 @@ class DQNAgent:
         model = Sequential()
 
         model.add(Dense(HIDDEN_LAYER_SIZE, input_dim=self.state_space, activation='relu'))
-        # model.add(Dropout(0.2))
         model.add(Dense(HIDDEN_LAYER_SIZE, activation='relu'))
-        # model.add(Dropout(0.2))
         # Output Layer with # of actions: 2 nodes (left, right)
         model.add(Dense(self.action_space, activation='linear'))
 
@@ -64,10 +61,13 @@ class DQNAgent:
             action_value = self.model.predict(state)
             action = np.argmax(action_value)  # Exploit
         return action
+    
+    def bellman(self, reward, next_state):
+        return reward + self.learning_rate * np.amax(self.model.predict(next_state)[0])
 
     def update_hyperparameters(self):
         if self.exploration > MIN_EXPLORATION:
-            self.exploration = self.exploration * 0.98
+            self.exploration = self.exploration * 0.995
 
 
 class SimpleAgent(DQNAgent):
@@ -80,10 +80,9 @@ class SimpleAgent(DQNAgent):
         DQNAgent.__init__(self, state_space, action_space)
 
     def update_model(self, state, action, reward, next_state, done):
+        q_update = -reward if done else self.bellman(reward, next_state)
         target = self.model.predict(state)
-        next_max = np.max(self.model.predict(next_state))
-
-        target[0][action] = reward + self.discount_factor * next_max
+        target[0][action] = q_update
 
         self.model.fit(state, target, epochs=1, verbose=0)
 
@@ -96,15 +95,10 @@ class BatchAgent(DQNAgent):
     def __init__(self, state_space, action_space):
         DQNAgent.__init__(self, state_space, action_space)
 
-        self.memory = deque(maxlen=MEMORY_SIZE)
+        self.memory = deque(maxlen=MINIBATCH_SIZE)
 
     def update_model(self, state, action, reward, next_state, done):
-        target = self.model.predict(state)
-        next_max = np.max(self.model.predict(next_state))
-
-        target[0][action] = reward + self.discount_factor * next_max
-
-        self.memory.append((state, target))
+        self.memory.append((state, action, reward, next_state, done))
 
     @staticmethod
     def supports_replay():
@@ -112,16 +106,23 @@ class BatchAgent(DQNAgent):
 
     def replay(self):
         iter = reversed(self.memory)
-        #inputs = np.empty_like(self.memory[0][0])
-        #targets = np.empty_like(self.memory[0][1])
-        inputs = np.empty((MINIBATCH_SIZE, self.state_space))
-        targets = np.empty((MINIBATCH_SIZE, self.action_space))
-        for _ in range(len(self.memory) if len(self.memory) < MINIBATCH_SIZE else MINIBATCH_SIZE):
-            record = next(iter)
-            np.append(inputs, record[0])
-            np.append(targets, record[1])
+        batch_size = min(len(self.memory), MINIBATCH_SIZE)
 
-        self.model.train_on_batch(inputs, targets)
+        #inputs = np.empty((batch_size, self.state_space))
+        #targets = np.empty((batch_size, self.action_space))
+        for _ in range(batch_size):
+            state, action, reward, next_state, done = next(iter)
+
+            q_update = -reward if done else self.bellman(reward, next_state)
+            target = self.model.predict(state)
+            target[0][action] = q_update
+
+            self.model.fit(state, target, verbose=0)
+
+            #np.append(inputs, state)
+            #np.append(targets, target)
+
+        #self.model.train_on_batch(inputs, targets)
 
 
 class RandomBatchAgent(DQNAgent):
