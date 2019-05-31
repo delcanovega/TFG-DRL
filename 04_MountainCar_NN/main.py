8ce9d817bd91bdffa3c9b7538ec6208e9e41d753
Ricardo <ricardo.arranz279@outlook.com>
2019-05-31 22:19:20 +0200

    Final Commit

--- 04_MountainCar_NN/main.py
+++ 04_MountainCar_NN/main.py
@@ -6,45 +6,103 @@ import copy
 from collections import deque
 
 # Comment this to launch on server
-import drl_agent
-from drl_agent_1 import RandomBatchAgent
-from drl_agent import RandomBatchAgentTwoBrains
 
-EPISODES = 100
+from drl_agent import RandomBatchAgentTwoBrainsBestSave #using Adelta as optimizer
+
+
+EPISODES = 10
 
 COMP = 10    # Every COMP episodes the agent and apprentice are compared
-BESTOF = 20  # Number of simulations of the comparison
 
-def simulate(env, agent):
-    scores = deque(maxlen=100)
-    performance = []
-    agent.load('modelos/MontainCar/modelo.h5')
+# Each episode the reward recived will be reduce to reforce the fastest way to reach our goal
+PENATYFORSLOW = 0.999
 
-    #this is realy important because almost all metods(4) has been changed in RandomBatchAgentTwoBrains
-    use_apprentice = True if (isinstance(agent, RandomBatchAgentTwoBrains)) else False 
+
+# basada en la VELOCIDAD ABSOLUTA * 100
+
+def represent(arr, mt):
+    return arr[mt]
+
+t = np.arange(EPISODES)
+
+def ourReward(state):
+    reward = abs(state[0][1]) * 100
+    return reward
+
+def subSimulate(env, agent, k):
+    state = env.reset()
+    state = np.reshape(state, [1, env.observation_space.shape[0]])
+    done = False
+    acc_Reward = 0
+    steps = 0
+    while not done:
+        action = agent.optimal_action(state, k)
+
+        next_state, reward, done, _ = env.step(action)
+        steps += 1
+        next_state = np.reshape(
+            next_state, [1, env.observation_space.shape[0]])
+
+        if done and steps < 200:
+            acc_Reward = 1000 - steps
+        else:
+            reward = ourReward(state)
+
+        state = next_state
+        acc_Reward += reward
+
+    return acc_Reward
+
+def simulate(env, agent, use_apprentice=False):
+    oldScores = deque(maxlen=100) 
+    performance, scores, real_scores, maxPos, apprenticeScores, bestScores = [[] for _ in range(6)]
+    
+    use_apprentice = True if (isinstance(agent, RandomBatchAgentTwoBrainsBestSave)) else False 
 
     for i in range(EPISODES):
         state = env.reset()
         state = np.reshape(state, [1, env.observation_space.shape[0]])
 
         # Do not compete on the first episode
-        if use_apprentice and i % COMP == 0 and i >= COMP:
-            agent.compete(env)
-            
+        if use_apprentice and i % COMP == 0:
+            if i % 100 == 0:
+                agent.bigCompete(env)
+            else :
+                agent.compete(env)
+
+            state = env.reset()
+            state = np.reshape(state, [1, env.observation_space.shape[0]])
+
         done = False
         acc_reward = 0
+        acc_reward_environment = 0
+        maxp = 0
+
+        step = 0
         while not done:
             if i > 00:
-               env.render()
+                env.render()
 
             action = agent.predict_action(state)
 
             next_state, reward, done, _ = env.step(action)
-            next_state = np.reshape(next_state, [1, env.observation_space.shape[0]])
+            step += 1
+            acc_reward_environment += reward
+          
+            if maxp < state[0][0]:
+                maxp = state[0][0]
+
+            if done and step < 200:
+                acc_reward = 1000 - step
+            else:
+                reward = ourReward(state)
+
             
-            #now Mentor and Apprentice share memory
+            next_state = np.reshape(
+                next_state, [1, env.observation_space.shape[0]])
+
             agent.update_model(state, action, reward, next_state, done)
-           
+
             if use_apprentice and i < COMP:
                 agent.replay_Mentor()
             elif agent.supports_replay():
@@ -52,45 +110,87 @@ def simulate(env, agent):
 
             state = next_state
             acc_reward += reward
-
+        apprenticeReward = subSimulate(env, agent, 1)
+        bestReward = subSimulate(env, agent, 2)
         agent.update_hyperparameters()
-
+        
+        oldScores.append(acc_reward)
         scores.append(acc_reward)
-        performance.append(np.mean(scores))
+        real_scores.append(acc_reward_environment)
+        maxPos.append(maxp)
+        performance.append(np.mean(oldScores))
+        apprenticeScores.append(apprenticeReward)
+        bestScores.append(bestReward)
         print("Episode {}/{} score {}".format(i + 1, EPISODES, int(acc_reward)))
+    
+    acc_mentor, acc_apprentice, acc_best = agent.finalTrial(env)
 
-    agent.save('modelos/MontainCar/modelo.h5')
-
-    return performance
+    return scores, real_scores, maxPos, performance, acc_mentor, acc_apprentice, acc_best, apprenticeScores, bestScores
 
 
-def create_plot(results):
-    colors = ['blue', 'purple', 'pink']
+def saveScores(scores, real_scores, maxPos, performance, acc_mentor, acc_apprentice, acc_best, apprenticeScores, bestScores):
+    np.savetxt('Scores.txt',scores)
+    np.savetxt('Enviroment_Scores.txt', real_scores)
+    np.savetxt('MaxPos.txt',maxPos)
+    np.savetxt('Performance.txt',performance)
+    np.savetxt('apprenticeScores.txt',apprenticeScores)
+    np.savetxt('bestScores.txt',bestScores)
+    np.savetxt('acc_mentor.txt',acc_mentor)
+    np.savetxt('acc_apprentice.txt',acc_apprentice)
+    np.savetxt('acc_best.txt',acc_best)
 
-    _, ax = plt.subplots()
-    ax.set(xlabel='Episodio', ylabel='Media de todos los episodios')
-    ax.grid()
+def loadScores():
+    
+    scores = np.loadtxt('Scores.txt')
+    real_scores = np.loadtxt("Enviroment_Scores.txt")
+    maxPos = np.loadtxt("MaxPos.txt")
+    performance = np.loadtxt("Performance.txt")
+    apprenticeScores = np.loadtxt('apprenticeScores.txt')
+    bestScores = np.loadtxt('bestScores.txt')
+    acc_mentor = np.loadtxt('acc_mentor.txt')
+    acc_apprentice = np.loadtxt('acc_apprentice.txt')
+    acc_best = np.loadtxt('acc_best.txt')
 
-    ax.plot([195 for i in range(len(results[0]))], color='green')
-    for i in range(len(results)):
-        # results[i] = list(map(lambda x: 200 if x > 200 else x, results[i]))  # Limit the performance to 200
-        ax.plot(results[i], color=colors[i])
+    return scores, real_scores, maxPos, performance, acc_mentor, acc_apprentice, acc_best, apprenticeScores, bestScores
 
+def create_mega_plot(scores, real_scores, maxPos, performance, acc_mentor, acc_apprentice, acc_best, apprenticeScores, bestScores):
+    colors = ['blue', 'red', 'purple', 'green', 'pink']
+
+    ax = plt.subplot(211)
+    ax.set(xlabel='Episodes')
+    ax.plot([800 for i in range(len(scores))], 'm--', label = 'Goal')
+    ax.plot(range(len(scores)),apprenticeScores, color = 'cyan', label = 'Apprentice Scores')
+    ax.plot(range(len(scores)),bestScores, color = colors[3], label = 'Best Scores')
+    ax.plot(range(len(scores)),scores, color=colors[0], label = 'Scores')
+    ax.plot(range(len(real_scores)),real_scores, color=colors[1], label = 'Enviroment Scores')
+    ax.plot(range(len(scores)), performance, color = 'black', label = '100 mean scores')
+    ax.legend()
+
+    a3x = plt.subplot(223)
+    a3x.set(xlabel='Episodes')
+    a3x.plot(range(len(maxPos)),maxPos, color=colors[3], label = 'Maximum position')
+    a3x.legend()
+
+    a5x = plt.subplot(224)
+    a5x.set(xlabel='FINAL TRIAL')
+    a5x.plot(range(len(acc_mentor)),acc_mentor, color=colors[0], label = 'FINAL MENTOR')
+    a5x.plot(range(len(acc_apprentice)),acc_apprentice, color=colors[1], label = 'FINAL APPRENTICE')
+    a5x.plot(range(len(acc_best)),acc_best, color=colors[3], label = 'FINAL BEST')
+    a5x.legend()
     return plt
 
-
 if __name__ == '__main__':
+    
     env = gym.make('MountainCar-v0')
+    
+    scores, real_scores, maxPos, performance, acc_mentor, acc_apprentice, acc_best, apprenticeScores, bestScores = simulate(env,
+     RandomBatchAgentTwoBrainsBestSave(env.observation_space.shape[0], env.action_space.n))
 
-    results = []
-    # Uncomment the agent that you want to test
-    #results.append(simulate(env, SimpleAgent(env.observation_space.shape[0], env.action_space.n)))
-    #results.append(simulate(env, BatchAgent(env.observation_space.shape[0], env.action_space.n)))
-    results.append(simulate(env, RandomBatchAgent(env.observation_space.shape[0], env.action_space.n)))
-    #results.append(simulate(env, RandomBatchAgent(env.observation_space.shape[0], env.action_space.n), True))
-    #results.append(simulate(env, RandomBatchAgentTwoBrains(env.observation_space.shape[0], env.action_space.n)))
+    #save scores
+    #saveScores(scores, real_scores, maxPos, performance, acc_mentor, acc_apprentice, acc_best, apprenticeScores, bestScores)
 
-
-    # Plot the results
-    plt = create_plot(results)
+    #load scores
+    #scores, real_scores, maxVel, maxPos, velAcc, posAcc, performance, competes,  bestchangedPos, bestchangedVal, acc_mentor, acc_apprentice, acc_best, apprenticeScores, bestScores = loadScores()
+    
+    plt = create_mega_plot(scores, real_scores, maxPos, performance, acc_mentor, acc_apprentice, acc_best, apprenticeScores, bestScores)
     plt.show()
